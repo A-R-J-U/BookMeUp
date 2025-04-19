5d2a74a06c9174fa905b38ed3ce7ee2e7270e0c9
ARJUN KAYMALA <shirazdxb.ss@gmail.com>
2025-04-20 05:14:55 +0530

    Update auth.js

--- Backend/Controllers/auth.js
+++ Backend/Controllers/auth.js
@@ -84,6 +84,7 @@ const signin = async (req, res) => {
         secure: process.env.NODE_ENV === "production",
         sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
         maxAge: 24 * 60 * 60 * 1000, // 1 day
+        path: '/',
       }
       )
       .json({ message: "User signed in successfully", token: token });
